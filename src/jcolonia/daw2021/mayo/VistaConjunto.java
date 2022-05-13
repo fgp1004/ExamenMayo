63a0bdd64d7b94ce4856d7467feb0d79fb1197b7
Fernando Gómez <fernando.gomper@educa.jcyl.es>
2022-05-13 10:09:41 +0200

    Imprime posiciones de la lista pero formato incorrecto

--- src/jcolonia/daw2021/mayo/VistaConjunto.java
+++ src/jcolonia/daw2021/mayo/VistaConjunto.java
@@ -9,12 +9,11 @@ public class VistaConjunto extends Vista{
 	}
 
 	public void mostrarListado(ListaNúmeros conjunto) {
-		//for(int i =0;i<conjunto.getNúmSumandos();i++) {
-			conjunto.toListaString();
-		//}
 		
+		
+		for(int i =0;i<conjunto.getNúmSumandos();i++) {
+			System.out.printf("%3.d → %f\n",i+1,conjunto.toListaString().get(i) );
+		}
 	}
-
-
 	
 }
