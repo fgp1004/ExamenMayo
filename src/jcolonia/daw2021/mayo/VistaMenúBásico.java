df70ca6d111aa6c4de42d0932aee2e614a9c2457
Fernando Gómez <fernando.gomper@educa.jcyl.es>
2022-05-13 08:46:30 +0200

    Implementación de métodos

--- src/jcolonia/daw2021/mayo/VistaMenúBásico.java
+++ src/jcolonia/daw2021/mayo/VistaMenúBásico.java
@@ -1,5 +1,50 @@
 package jcolonia.daw2021.mayo;
 
-public class VistaMenúBásico {
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
+public class VistaMenúBásico extends Vista{
+
+	private String[] opcionesMenúPrincipal;
+
+	public VistaMenúBásico(String texto, Scanner entrada, String[] opcionesMenúPrincipal) {
+		super(texto, entrada);
+		this.opcionesMenúPrincipal=opcionesMenúPrincipal;
+	}
+
+	public void mostrarOpciones() {
+		out.println("----------------------------------------");
+		for (int i = 0; i < opcionesMenúPrincipal.length; i++) {
+			out.printf("Opcion %d .- %s\n", i + 1, opcionesMenúPrincipal[i]);
+
+		}
+		out.println("----------------------------------------\n");
+	}
+
+	public int pedirOpción() {
+		String lineaTexto;
+		int número = 0;
+		boolean numEsCorrecto = false;
+		
+		while (!numEsCorrecto) {
+			try {
+				lineaTexto = getEntrada().nextLine();
+				número = Integer.parseInt(lineaTexto);
+				if (número < 0 || número > opcionesMenúPrincipal.length) {
+					out.println("No ha elegido una opcion valida.");
+					throw new NumberFormatException("Valor introducido fuera de rango de opciones.\n");
+				} else {
+					numEsCorrecto = true;
+				}
+			} catch (NumberFormatException ex) {
+				out.printf("\tNo es una de las opciones validas: «%s»%n", ex.getMessage());
+			}
+		}
+
+		return número;
+	}
+	
+	
 
 }
