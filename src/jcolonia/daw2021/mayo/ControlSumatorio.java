1b044cfc1162f4cdbfc3763334a9f3b1b16437f3
Fernando Gómez <fernando.gomper@educa.jcyl.es>
2022-05-17 18:07:41 +0200

    Añadido comentario de prueba para GitHub

--- src/jcolonia/daw2021/mayo/ControlSumatorio.java
+++ src/jcolonia/daw2021/mayo/ControlSumatorio.java
@@ -3,71 +3,74 @@ package jcolonia.daw2021.mayo;
 import java.util.Scanner;
 
 /**
- * Gestión de números «decimales»: recogida y visualización de la suma.
+ * GestiÃ³n de nÃºmeros Â«decimalesÂ»: recogida y visualizaciÃ³n de la suma.
  * 
- * @versión 2022.3.1
- * @author <a href="dmartin.jcolonia@gmail.com">David H. Martín</a>
+ * @versiÃ³n 2022.3.1
+ * @author <a href="dmartin.jcolonia@gmail.com">David H. MartÃ­n</a>
  * 
  */
+
+/*Esto es un ejemplo del uso de commits para GitHub*/
+
 public class ControlSumatorio {
 	/**
-	 * Texto identificativo de las funciones de la aplicación que aparecerán en el
-	 * menú principal.
+	 * Texto identificativo de las funciones de la aplicaciÃ³n que aparecerÃ¡n en el
+	 * menÃº principal.
 	 */
-	private static final String[] OPCIONES_MENÚ_PRINCIPAL = { "Agregar valor", "Mostrar valores", "Mostrar suma",
+	private static final String[] OPCIONES_MENÃš_PRINCIPAL = { "Agregar valor", "Mostrar valores", "Mostrar suma",
 			"Restablecer", "SALIR" };
 
 	/**
-	 * Título de la aplicación. Se mostrará como encabezado del menú principal.
+	 * TÃ­tulo de la aplicaciÃ³n. Se mostrarÃ¡ como encabezado del menÃº principal.
 	 */
-	private static final String TÍTULO_MENÚ_PRINCIPAL = "Sumatorio";
+	private static final String TÃ�TULO_MENÃš_PRINCIPAL = "Sumatorio";
 
 	/**
-	 * Recurso asociado a la entrada estándar de la aplicación. Debe ser un objeto
-	 * único a compartir con las diferentes vistas creadas.
+	 * Recurso asociado a la entrada estÃ¡ndar de la aplicaciÃ³n. Debe ser un objeto
+	 * Ãºnico a compartir con las diferentes vistas creadas.
 	 */
 	private Scanner entrada;
 
-	private ListaNúmeros conjunto;
+	private ListaNÃºmeros conjunto;
 
-	private VistaMenúBásico menúPrincipal;
+	private VistaMenÃºBÃ¡sico menÃºPrincipal;
 
 	public ControlSumatorio(Scanner in) {
 		this.entrada = in;
-		conjunto = new ListaNúmeros();
+		conjunto = new ListaNÃºmeros();
 	}
 
 	private void buclePrincipal() throws SumatorioNumberException {
-		int opciónElegida;
+		int opciÃ³nElegida;
 		boolean fin = false;
 
-		menúPrincipal = new VistaMenúBásico(TÍTULO_MENÚ_PRINCIPAL, entrada, OPCIONES_MENÚ_PRINCIPAL);
+		menÃºPrincipal = new VistaMenÃºBÃ¡sico(TÃ�TULO_MENÃš_PRINCIPAL, entrada, OPCIONES_MENÃš_PRINCIPAL);
 
 		// Bucle general
 		do {
-			menúPrincipal.mostrarTítulo1();
-			menúPrincipal.mostrarOpciones();
-			opciónElegida = menúPrincipal.pedirOpción();
-			switch (opciónElegida) {
+			menÃºPrincipal.mostrarTÃ­tulo1();
+			menÃºPrincipal.mostrarOpciones();
+			opcionElegida = menuPrincipal.pedirOpcion();
+			switch (opcionElegida) {
 			case 5: // SALIR
 				fin = true;
-				Vista.mostrarAviso("¡¡¡A-D-I-O-S!!");
+				Vista.mostrarAviso("Â¡Â¡Â¡A-D-I-O-S!!");
 				break;
-			case 1: // Opción 1: Entrada datos
+			case 1: // OpciÃ³n 1: Entrada datos
 				cargarSumando();
 				break;
-			case 2: // Opción 2: Mostrar sumandos
+			case 2: // OpciÃ³n 2: Mostrar sumandos
 				mostrarSumandos();
 				break;
-			case 3: // Opción 3: Mostrar suma
+			case 3: // OpciÃ³n 3: Mostrar suma
 				mostrarSuma();
 				break;
-			case 4: // Opción 4: Reset
+			case 4: // OpciÃ³n 4: Reset
 				restablecer();
 				break;
-			default: // Opción no esperada: abortar
-				ejecutarGenérico(opciónElegida);
-				System.err.println("Error interno de programa - operación pendiente de desarrollo");
+			default: // OpciÃ³n no esperada: abortar
+				ejecutarGenÃ©rico(opciÃ³nElegida);
+				System.err.println("Error interno de programa - operaciÃ³n pendiente de desarrollo");
 				System.exit(1);
 			}
 		} while (!fin);
@@ -75,17 +78,17 @@ public class ControlSumatorio {
 
 
 	/**
-	 * Añade valores tipo double a la lista 
+	 * AÃ±ade valores tipo double a la lista 
 	 * 
 	 * @throws SumatorioNumberException
 	 */
 	private void cargarSumando() throws SumatorioNumberException {
-		VistaAgregar añadir=new VistaAgregar("Introduzca un nuevo valor",entrada);
-		double numeroAñadir=0.0;
-		double numeroAñadido;			
+		VistaAgregar aÃ±adir=new VistaAgregar("Introduzca un nuevo valor",entrada);
+		double numeroAÃ±adir=0.0;
+		double numeroAÃ±adido;			
 		
-		numeroAñadido=añadir.agregarValor(numeroAñadir);
-		conjunto.add(numeroAñadido);
+		numeroAÃ±adido=aÃ±adir.agregarValor(numeroAÃ±adir);
+		conjunto.add(numeroAÃ±adido);
 	}
 	
 	
@@ -110,23 +113,23 @@ public class ControlSumatorio {
 	 * Inicializa la lista para eliminar el contenido de antes
 	 */
 	private void restablecer() {
-		conjunto = new ListaNúmeros();
+		conjunto = new ListaNÃºmeros();
 	}
 	
 	/**
-	 * Muestra un texto indicando la opción elegida por el usuario
+	 * Muestra un texto indicando la opciÃ³n elegida por el usuario
 	 * 
 	 * @param id
 	 */
-	private void ejecutarGenérico(int id) {
+	private void ejecutarGenÃ©rico(int id) {
 		String mensaje;
-		mensaje = String.format("%n  Ha elegido la opción %d: «%s»", id, OPCIONES_MENÚ_PRINCIPAL[id - 1]);
+		mensaje = String.format("%n  Ha elegido la opciÃ³n %d: Â«%sÂ»", id, OPCIONES_MENÃš_PRINCIPAL[id - 1]);
 		Vista.mostrarTexto(mensaje);
 	}
 	
 	
 	/**
-	 * Método main 
+	 * MÃ©todo main 
 	 * 
 	 * @param args
 	 * @throws SumatorioNumberException
